9770c4210def3b444f43b019e85f7ea90c18700e
_YoSoyHomS <68291324+homsdev@users.noreply.github.com>
2024-02-13 14:20:24 -0600

    Initial commit

---
+++ src/main/java/com/homs4j/expendlr/app/Application.java
@@ -0,0 +1,13 @@
+package com.homs4j.expendlr.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
